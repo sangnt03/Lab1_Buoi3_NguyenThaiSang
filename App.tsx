448ce5b83a5d7fb7523d01ff68f9d22a1c049766
sangnt03 <167545723+sangnt03@users.noreply.github.com>
2025-04-19 21:47:49 +0700

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Navigation from './src/Stack/Navigator/Navigation';
 
 function App(): React.JSX.Element {
-  return <Navigation />;
+  return <Navigation/>;
 }
 
 export default App;
